b7d69279ec948da3f7d006732e3189775a7d0f31
Kamil Bednarek <kamil.bednarek.2001@gmail.com>
2022-10-30 17:10:09 +0100

    Axios config Vue3 instead of 2

--- src/main.js
+++ src/main.js
@@ -2,5 +2,6 @@ import './plugins/axios'
 import {createApp} from 'vue'
 import App from './App.vue'
 import router from './router'
+import axios from './plugins/axios'
 
-createApp(App).use(router).mount('#app')
+createApp(App).use(router).use(axios).mount('#app')

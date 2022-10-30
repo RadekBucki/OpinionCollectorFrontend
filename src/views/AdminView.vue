0b4ae216fbfbc472ae616bea003aed6d5b89dbfb
Kamil Bednarek <kamil.bednarek.2001@gmail.com>
2022-10-30 17:11:35 +0100

    Admin and home page same

--- src/views/AdminView.vue
+++ src/views/AdminView.vue
@@ -1,5 +1,18 @@
 <template>
   <div class="admin">
     <h1>This is an admin page</h1>
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App view" />
   </div>
 </template>
+
+<script>
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'AdminView',
+  components: {
+    HelloWorld
+  }
+}
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="home">
+    <h1>This is an home page</h1>
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App view" />
   </div>

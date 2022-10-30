a7e1c5b2b08ad731b900872059e268f2fbac25d2
Radosław Bucki <236507@edu.p.lodz.pl>
2022-10-30 10:10:11 +0100

    Add axios

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -9,50 +9,50 @@ import axios from "axios";
 // axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
 
 let config = {
-  baseURL: process.env.BACKEND_URL || "",
-  timeout: 60 * 1000, // Timeout
+    baseURL: process.env.BACKEND_URL || "",
+    timeout: 60 * 1000, // Timeout
 };
 
 const _axios = axios.create(config);
 
 _axios.interceptors.request.use(
-  function(config) {
-    // Do something before request is sent
-    return config;
-  },
-  function(error) {
-    // Do something with request error
-    return Promise.reject(error);
-  }
+    function (config) {
+        // Do something before request is sent
+        return config;
+    },
+    function (error) {
+        // Do something with request error
+        return Promise.reject(error);
+    }
 );
 
 // Add a response interceptor
 _axios.interceptors.response.use(
-  function(response) {
-    // Do something with response data
-    return response;
-  },
-  function(error) {
-    // Do something with response error
-    return Promise.reject(error);
-  }
+    function (response) {
+        // Do something with response data
+        return response;
+    },
+    function (error) {
+        // Do something with response error
+        return Promise.reject(error);
+    }
 );
 
-Plugin.install = function(Vue) {
-  Vue.axios = _axios;
-  window.axios = _axios;
-  Object.defineProperties(Vue.prototype, {
-    axios: {
-      get() {
-        return _axios;
-      }
-    },
-    $axios: {
-      get() {
-        return _axios;
-      }
-    },
-  });
+Plugin.install = function (Vue) {
+    Vue.axios = _axios;
+    window.axios = _axios;
+    Object.defineProperties(Vue.prototype, {
+        axios: {
+            get() {
+                return _axios;
+            }
+        },
+        $axios: {
+            get() {
+                return _axios;
+            }
+        },
+    });
 };
 
 Vue.use(Plugin)

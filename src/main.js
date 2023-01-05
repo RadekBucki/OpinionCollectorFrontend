d0a93a1018c95e6260b6d5f556f1b9e0a9aabb85
236493 Banachowicz Anna <236493@edu.p.lodz.pl>
2023-01-05 19:21:15 +0100

    Remove bootstrap script

--- src/main.js
+++ src/main.js
@@ -3,7 +3,9 @@ import {createApp} from 'vue'
 import App from './App.vue'
 import router from './router'
 import axios from './plugins/axios'
-import "bootstrap/dist/css/bootstrap.min.css"
+
 import "bootstrap"
+// import './helpers/Global.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 createApp(App).use(router).use(axios).mount('#app')

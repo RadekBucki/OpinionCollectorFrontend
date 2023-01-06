f7c68eb81721eb8205e1d144a059ce53637c0927
236493 Banachowicz Anna <236493@edu.p.lodz.pl>
2023-01-06 22:37:50 +0100

    Added components responsible for sidebar & mobile Modified components in userPanel to use them

---
+++ src/components/MobilePanelNavigation.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="mobile">
+    <ul class="list-group list-group-horizontal justify-content-center">
+      <li class="nav-item">
+        <RouterLink class="list-group-item" :class="itemClasses('panel')" :to="{ name: 'Panel' }">Data</RouterLink>
+      </li>
+      <li>
+        <RouterLink class="list-group-item" :class="itemClasses('opinions')" :to="{ name: 'Opinions' }"
+          >Opinions</RouterLink
+        >
+      </li>
+      <li>
+        <RouterLink class="list-group-item" :class="itemClasses('suggestions')" :to="{ name: 'Suggestions' }"
+          >Suggestions</RouterLink
+        >
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    itemClasses(route) {
+      const classes = ["nav-item"];
+      if (this.currentRoute === route) {
+        classes.push("active");
+      } else {
+        classes.push("link-dark");
+      }
+      return classes;
+    },
+  },
+  computed: {
+    currentRoute() {
+      return this.$route.name.toLowerCase();
+    },
+  },
+};
+</script>
+
+<style scoped>
+.mobile ul {
+  list-style: none;
+  margin-top: 1.4em;
+}
+
+@media (min-width: 1150px) {
+  .mobile ul {
+    display: none;
+  }
+}
+</style>
---
+++ src/components/PanelNavigation.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="sidepanel d-flex flex-column flex-shrink-0 p-3 bg-light" style="width: 280px; height: 92vh">
+    <a class="mb-3 mb-md-0 me-md-auto link-dark text-decoration-none mx-auto">
+      <h1 class="fs-4">Dashboard</h1>
+    </a>
+    <hr />
+    <ul class="nav nav-pills flex-column mb-auto">
+      <li class="nav-item">
+        <RouterLink class="nav-link" :class="itemClasses('panel')" :to="{ name: 'Panel' }">Data</RouterLink>
+      </li>
+      <li>
+        <RouterLink class="nav-link" :class="itemClasses('opinions')" :to="{ name: 'Opinions' }">Opinions</RouterLink>
+      </li>
+      <li>
+        <RouterLink class="nav-link" :class="itemClasses('suggestions')" :to="{ name: 'Suggestions' }"
+          >Suggestions</RouterLink
+        >
+      </li>
+    </ul>
+    <hr />
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    itemClasses(route) {
+      const classes = ["nav-item"];
+      if (this.currentRoute === route) {
+        classes.push("active");
+      } else {
+        classes.push("link-dark");
+      }
+      return classes;
+    },
+  },
+  computed: {
+    currentRoute() {
+      return this.$route.name.toLowerCase();
+    },
+  },
+};
+</script>
+
+<style scoped>
+@media (max-width: 1150px) {
+  .sidepanel {
+    display: none !important;
+  }
+}
+</style>

264ba0063799faa392dde99c5ef079da0ef4b254
236493 Banachowicz Anna <236493@edu.p.lodz.pl>
2023-01-06 13:22:02 +0100

    Made pages in userPanel responsive

--- src/views/UserPanel/DataPage.vue
+++ src/views/UserPanel/DataPage.vue
@@ -1,6 +1,19 @@
 <template>
+  <div class="mobile mt-3">
+    <ul class="list-group list-group-horizontal justify-content-center">
+      <li class="nav-item">
+        <RouterLink class="list-group-item active" :to="{ name: 'Panel' }">Data</RouterLink>
+      </li>
+      <li>
+        <RouterLink class="list-group-item link-dark" :to="{ name: 'Opinions' }">Opinions</RouterLink>
+      </li>
+      <li>
+        <RouterLink class="list-group-item link-dark" :to="{ name: 'Suggestions' }">Suggestions</RouterLink>
+      </li>
+    </ul>
+  </div>
   <div class="row mx-0">
-    <div class="d-flex flex-column flex-shrink-0 p-3 bg-light" style="width: 280px; height: 92vh">
+    <div class="sidepanel d-flex flex-column flex-shrink-0 p-3 bg-light" style="width: 280px; height: 92vh">
       <a class="mb-3 mb-md-0 me-md-auto link-dark text-decoration-none mx-auto">
         <h1 class="fs-4">Dashboard</h1>
       </a>
@@ -19,11 +32,11 @@
       <hr />
     </div>
 
-    <div class="col-9 mt-3">
+    <div class="form col-9 mt-3">
       <h1>User Data</h1>
 
       <div class="avatarCircle col-4 mt-4 mx-auto">
-        <label for="" class="form-label text-dark mx-4">Avatar Image</label>
+        <label class="form-label text-dark mx-4">Avatar Image</label>
         <img
           src="https://images.unsplash.com/photo-1536164261511-3a17e671d380?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=682&q=80"
           alt="Avatar Profile"
@@ -32,22 +45,22 @@
       </div>
       <div class="mt-5 d-flex justify-content-center align-items-center">
         <form class="col-9">
-          <div class="row">
+          <div class="row form-mobile">
             <div class="col-4">
-              <label for="firstNameForm" class="form-label text-dark">Name</label>
+              <label class="form-label text-dark">Name</label>
               <input type="text" v-model="firstName" class="form-control" placeholder="Name" />
             </div>
             <div class="col-6">
-              <label for="lastNameForm" class="form-label text-dark">Last name</label>
+              <label class="form-label text-dark">Last name</label>
               <input type="text" v-model="lastName" class="form-control" placeholder="Last name" />
             </div>
           </div>
-          <div class="row mt-2">
+          <div class="row form-mobile mt-2">
             <div class="col-7">
-              <label for="emailForm" class="form-label text-dark">e-mail</label>
+              <label class="form-label text-dark">Address e-mail</label>
               <input type="text" v-model="email" class="form-control" placeholder="e-mail" />
             </div>
-            <div class="col-3 mt-4 d-flex justify-content-end">
+            <div class="mt-2 d-flex justify-content-end">
               <button class="btn btn-primary">Save changes</button>
             </div>
           </div>
@@ -74,5 +87,29 @@ export default {
 <style scoped>
 .avatarCircle img {
   border-radius: 50%;
+}
+.mobile ul {
+  list-style: none;
+}
+
+@media (min-width: 1150px) {
+  .mobile ul {
+    display: none;
+  }
+}
+
+@media (max-width: 1150px) {
+  .sidepanel {
+    display: none !important;
+  }
+  .form {
+    width: 100%;
+  }
+  .form-mobile {
+    flex-direction: column !important;
+  }
+  .form-mobile div {
+    width: 100%;
+  }
 }
 </style>

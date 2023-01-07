be637c2fd8934e425b77698c4b74d287ffd9b4f5
Tomasz Roske <236639@edu.p.lodz.pl>
2023-01-07 14:48:51 +0100

    add endpoint path & backend url

---
+++ src/communication/EnumEndpoints.ts
@@ -0,0 +1,38 @@
+export enum Global {
+    BASE_URL = "http://localhost:8080",
+}
+
+export enum Get {
+    CATEGORIES = "/categories",
+    CATEGORIES_ALL = "/categories/all",
+    OPINIONS_PRODUCT = "/opinions/product",
+    OPINIONS_USER = "/opinions/user",
+    PRODUCTS = "/products/",
+    PRODUCTS_ALL = "/products/all/",
+    PRODUCTS_DETAILS = "/products/details",
+    PRODUCT_SEARCH = "/products/search",
+    SUGGESTIONS_ALL = "/suggestions/get",
+    SUGGESTIONS_USER = "/suggestions/user",
+    USERS_ALL = "/users"
+}
+
+export enum Post {
+    CATEGORIES_ADD = "/categories/add",
+    OPINIONS_ADD = "/opinions/add",
+    PRODUCTS_ADD = "/products/add",
+    SUGGESTIONS_ADD = "/suggestions/add",
+    USER_LOGIN = "/users/login",
+    USER_REGISTER = "/users/register"
+}
+
+export enum Put {
+    CATEGORIES_EDIT = "/categories/edit",
+    PRODUCTS_EDIT = "/products/edit",
+    SUGGESTIONS_REPLY = "/suggestions/reply",
+    USER_EDIT = "/users/update"
+}
+
+export enum Delete {
+    CATEGORIES_DELETE = "/categories/delete",
+    PRODUCTS_DELETE = "/products/delete"
+}

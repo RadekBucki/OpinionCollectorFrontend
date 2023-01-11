604b1ad508abc1a1877de0ac82a6fc38ce917eca
Pawel Dera <paweldera01@gmail.com>
2023-01-11 20:02:31 +0100

    Add register management

--- src/components/modals/RegisterModal.vue
+++ src/components/modals/RegisterModal.vue
@@ -8,25 +8,27 @@
         </div>
         <div class="modal-body">
           <form @submit.prevent="registerUser">
-            <div class="form-group mb-3">
-              <label >Email</label>
-              <input v-model="this.email" type="text" class="form-control" aria-describedby="emailHelp" placeholder="Enter email" required>
+            <div class="text-start form-group mb-3">
+              <label>Email</label>
+              <input v-model="this.credentials.email" type="email" class="form-control" placeholder="Enter email" required>
+              <small v-if="this.errors.accountError" class="text-start" style="color: red;">{{ this.errors.errorMessage }}</small>
             </div>
-            <div class="form-group mb-3">
-              <label >First name</label>
-              <input v-model="this.firstName" type="password" class="form-control" placeholder="Enter first name" required>
+            <div class="text-start form-group mb-3">
+              <label>First name</label>
+              <input v-model="this.credentials.firstName" type="text" class="form-control" placeholder="Enter first name" required>
             </div>
-            <div class="form-group mb-3">
-              <label >Last name</label>
-              <input v-model="this.lastName" type="password" class="form-control" placeholder="Enter last name" required>
+            <div class="text-start form-group mb-3">
+              <label>Last name</label>
+              <input v-model="this.credentials.lastName" type="text" class="form-control" placeholder="Enter last name" required>
             </div>
-            <div class="form-group mb-3">
-              <label >Password</label>
-              <input v-model="this.password" type="password" class="form-control" placeholder="Enter password" required>
+            <div class="text-start form-group mb-3">
+              <label>Password</label>
+              <input v-model="this.credentials.password" type="password" class="form-control" placeholder="Enter password" required>
+              <small v-if="this.errors.passwordError" class="text-start" style="color: red;">Passwords don't match!</small>
             </div>
-            <div class="form-group mb-3">
-              <label >Confirm password</label>
-              <input v-model="this.confirmPassword" type="password" class="form-control" placeholder="Enter password again" required>
+            <div class="text-start form-group mb-3">
+              <label>Confirm password</label>
+              <input v-model="this.credentials.confirmPassword" type="password" class="form-control" placeholder="Enter password again" required>
             </div>
             <button type="submit" class="btn btn-primary m-auto w-100">Register</button>
           </form>
@@ -35,16 +37,15 @@
     </div>
   </div>
 
-  <div class="modal fade" ref="staticBackdropForError" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+  <div class="modal fade" ref="staticBackdropForMessage" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-hidden="true">
     <div class="modal-dialog modal-dialog-centered">
       <div class="modal-content">
         <div class="modal-header">
-          <h1 class="modal-title fs-5" id="staticBackdropLabel">Error!</h1>
+          <h1 class="modal-title fs-5" id="staticBackdropLabel">Success!</h1>
           <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
         </div>
         <div class="modal-body">
-          <h3>Error occurred</h3>
-          <p>Error message</p>
+          <p>You have successfully registered your account!</p>
         </div>
       </div>
     </div>
@@ -53,29 +54,65 @@
 
 <script>
 import {Modal} from "bootstrap";
+import {PostRequest} from "@/communication/Network.ts";
 
 export default {
   emits: ['get-modal'],
   data() {
     return {
       modal: null,
-      errorModal: null,
-      email: null,
-      password: null,
-      confirmPassword: null,
-      firstName: null,
-      lastName: null,
-      imageUrl: null,
+      messageModal: null,
+      credentials: {
+        email: null,
+        password: null,
+        confirmPassword: null,
+        firstName: null,
+        lastName: null,
+        imageUrl: null,
+      },
+      errors: {
+        errorMessage: null,
+        accountError: false,
+        passwordError: false,
+      }
     };
   },
   methods: {
-    async registerUser(e) {
-      e.preventDefault();
+    async registerUser() {
+      if(this.credentials.password !== this.credentials.confirmPassword) {
+        this.errors.passwordError = true;
+        return;
+      } else {
+        this.errors.passwordError = false;
+      }
+
+      const registerUser = {
+        email: this.credentials.email,
+        firstName: this.credentials.firstName,
+        lastName: this.credentials.lastName,
+        password: this.credentials.password,
+        pictureUrl: "https://www.url-to-picture.com",
+        isAdmin: false,
+        id: null
+      };
+
+      PostRequest.userRegister(registerUser).then(() => {
+        this.errors.accountError = false
+        this.modal.hide()
+        this.messageModal.show()
+      }).catch((err) => {
+        if (err.response.status === 406) {
+          this.errors.errorMessage = `This email is already taken!`
+        } else {
+          this.errors.errorMessage = `Sorry! We couldn't register your account!`
+        }
+        this.errors.accountError = true
+      })
     },
   },
   mounted() {
     this.modal = new Modal(this.$refs.staticBackdropForRegister)
-    this.errorModal = new Modal(this.$refs.staticBackdropForError)
+    this.messageModal = new Modal(this.$refs.staticBackdropForMessage)
     this.$emit('get-modal', this.modal)
   }
 };

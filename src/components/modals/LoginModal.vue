606f22f678d2e5dacb4ebe10e16adc01a3efff26
Pawel Dera <paweldera01@gmail.com>
2023-01-11 19:17:29 +0100

    Add login management

--- src/components/modals/LoginModal.vue
+++ src/components/modals/LoginModal.vue
@@ -8,13 +8,14 @@
         </div>
         <div class="modal-body">
           <form @submit.prevent="loginUser">
-            <div class="form-group mb-3">
-              <label >Email</label>
-              <input v-model="this.email" type="text" class="form-control" aria-describedby="emailHelp" placeholder="Enter email" required>
+            <div class="form-group text-start mb-3">
+              <label>Email</label>
+              <input v-model="this.credentials.email" type="email" class="form-control" aria-describedby="emailHelp" placeholder="Enter email" required>
+              <small v-if="loginError" class="text-start" style="color: red;">Incorrect login or password!</small>
             </div>
-            <div class="form-group mb-3">
-              <label >Password</label>
-              <input v-model="this.password" type="password" class="form-control" placeholder="Enter password" required>
+            <div class="form-group text-start mb-3">
+              <label>Password</label>
+              <input v-model="this.credentials.password" type="password" class="form-control" placeholder="Enter password" required>
             </div>
             <button type="submit" class="btn btn-primary m-auto w-100">Log in</button>
           </form>
@@ -23,16 +24,15 @@
     </div>
   </div>
 
-  <div class="modal fade" ref="staticBackdropForError" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+  <div class="modal fade" ref="staticBackdropForMessage" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
     <div class="modal-dialog modal-dialog-centered">
       <div class="modal-content">
         <div class="modal-header">
-          <h1 class="modal-title fs-5" id="staticBackdropLabel">Error!</h1>
+          <h1 class="modal-title fs-5" id="staticBackdropLabel">Success!</h1>
           <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
         </div>
         <div class="modal-body">
-          <h3>Error occurred</h3>
-          <p>Error message</p>
+          <p>You have successfully logged in!</p>
         </div>
       </div>
     </div>
@@ -41,25 +41,35 @@
 
 <script>
 import {Modal} from "bootstrap";
+import {PostRequest} from "@/communication/Network.ts";
 
 export default {
   emits: ['get-modal'],
   data() {
     return {
       modal: null,
-      errorModal: null,
-      email: null,
-      password: null,
+      messageModal: null,
+      loginError: false,
+      credentials: {
+        email: null,
+        password: null,
+      }
     };
   },
   methods: {
-    async loginUser(e) {
-      e.preventDefault();
+    async loginUser() {
+      PostRequest.userLogin(this.credentials.email, this.credentials.password).then(() => {
+        this.loginError = false;
+        this.modal.hide();
+        this.messageModal.show();
+      }).catch(() => {
+        this.loginError = true;
+      })
     },
   },
   mounted() {
     this.modal = new Modal(this.$refs.staticBackdropForLogin)
-    this.errorModal = new Modal(this.$refs.staticBackdropForError)
+    this.messageModal = new Modal(this.$refs.staticBackdropForMessage)
     this.$emit('get-modal', this.modal)
   }
 };

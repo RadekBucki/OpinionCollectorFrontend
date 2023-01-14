fe1c2234ae1ec58be7df494e2f8328a6d8a7a2ac
aniaxb <236493@edu.p.lodz.pl>
2023-01-14 19:11:50 +0100

    Fixed switching views after logging out Changed formatting in review and reviewer (suggestions) Advantages and disadvantages are displayed differently Added error 401 handling

--- src/views/UserPanel/OpinionsPage.vue
+++ src/views/UserPanel/OpinionsPage.vue
@@ -6,6 +6,7 @@
       <h1>User Opinions</h1>
       <div v-for="opinion in userOpinions" :key="opinion">
         <div class="mt-5 mx-auto border rounded item row text-start">
+          <div class="fw-semibold fs-5 mx-2 mt-1">Nazwa produktu</div>
           <div class="row px-3 box d-flex justify-content-center align-items-center">
             <div class="col-3 image d-flex justify-content-center align-items-center">
               <img :src="opinion.pictureUrl" class="img-fluid" alt="productImage" />
@@ -22,21 +23,30 @@
               <p v-if="!opinion.opinionValue" class="col-5">{{ "No opinions" }}</p>
             </div>
             <div class="col-3">
-              <div>
-                Advantages:
-                <ul v-for="advantage in opinion.advantages" :key="advantage">
-                  <li>{{ advantage }}</li>
-                </ul>
-              </div>
+            <div v-if="opinion.advantages" class="mt-3">
+            <b class="text-start fs-6" style="color:green;"> Advantages </b>
+            <div class="d-flex align-items-center" v-for="adv in opinion.advantages" :key="adv">
+              <b class="d-flex text-start fs-5" style="color:green;"> + </b>
+              <div class="text-start ms-1" style="color:green;">{{ adv }}</div>
             </div>
-            <div class="col-3">
-              <div>
-                Disadvantages:
-                <ul v-for="disadvantage in opinion.disadvantages" :key="disadvantage">
-                  <li>{{ disadvantage }}</li>
-                </ul>
-              </div>
+          </div>
+          <div v-if="!opinion.advantages" class="mt-3 text-start">
+            <b class="text-start fs-6" style="color:green;"> No advantages. </b>
+          </div>
+        </div>
+
+        <div class="col-3">
+          <div v-if="opinion.disadvantages" class="mt-3">
+            <b class="d-flex text-start fs-6" style="color:red;"> Disadvantages </b>
+            <div class="d-flex align-items-center" v-for="disadvantage in opinion.disadvantages" :key="disadvantage">
+              <b class="text-start fs-5" style="color:red;"> - </b>
+              <div class="text-start ms-1" style="color:red;">{{ disadvantage }}</div>
             </div>
+          </div>
+          <div v-if="!opinion.disadvantages" class="mt-3 text-start">
+            <b class="text-start fs-6" style="color:red;"> No disadvantages. </b>
+          </div>
+        </div>
           </div>
         </div>
       </div>
@@ -48,6 +58,7 @@
 import PanelNavigation from "@/components/PanelNavigation.vue";
 import MobilePanelNavigation from "@/components/MobilePanelNavigation.vue";
 import { GetRequest } from "@/communication/Network.ts";
+import { MethodRequest } from "@/communication/Network.ts";
 import StarRating from "vue-star-rating";
 
 export default {
@@ -69,7 +80,10 @@ export default {
           this.userOpinions = result;
         })
         .catch((err) => {
-          console.log(err.response);
+          if(err.response.status === 401) {
+            MethodRequest.userLogout();
+            window.location.reload();
+          }
         });
     },
   },
--- src/views/UserPanel/SuggestionsPage.vue
+++ src/views/UserPanel/SuggestionsPage.vue
@@ -12,20 +12,37 @@
             <div class="col-3 image d-flex justify-content-center">
               <img :src="suggestion.product.pictureUrl" class="img-fluid" alt="productImage" />
             </div>
-            <div class="col-4">
+            <div class="col-3">
               <h1 class="fs-6 fw-bold">Suggestion description:</h1>
               <div>
                 {{ suggestion.description }}
               </div>
             </div>
-            <div class="col-2">
+            <div class="col-3">
               <h1 class="fs-6 fw-bold">Reviewer:</h1>
-              <div v-if="suggestion.reviewer">{{ suggestion.reviewer }}</div>
+              <div v-if="suggestion.reviewer">
+                <div>
+                  <b>First name: </b><span>{{ suggestion.reviewer.firstName }}</span>
+                </div>
+               <div>
+                <b>Last name: </b><span>{{ suggestion.reviewer.lastName }}</span>
+               </div>
+                <div>
+                  <b>Profile: </b><img :src="suggestion.reviewer.profilePictureUrl" class="img-fluid" alt="profilePicture" />
+                </div>
+              </div>
               <div v-if="!suggestion.reviewer">Reviewer not assigned</div>
             </div>
-            <div class="col-2">
+            <div class="col-3">
               <h1 class="fs-6 fw-bold">Review:</h1>
-              <div v-if="suggestion.review">{{ suggestion.review }}</div>
+              <div v-if="suggestion.review">
+                <div>
+                  <b>Status: </b><span>{{ suggestion.review.status }}</span>
+                </div>
+                <div>
+                  <b>Reply: </b><span>{{ suggestion.review.reply }}</span>
+                </div>
+              </div>
               <div v-if="!suggestion.review">Review result not submitted yet</div>
             </div>
           </div>
@@ -39,6 +56,7 @@
 import PanelNavigation from "@/components/PanelNavigation.vue";
 import MobilePanelNavigation from "@/components/MobilePanelNavigation.vue";
 import { GetRequest } from "@/communication/Network.ts";
+import { MethodRequest } from "@/communication/Network.ts";
 
 export default {
   name: "SuggestionsPage",
@@ -58,7 +76,10 @@ export default {
           this.userSuggestions = result;
         })
         .catch((err) => {
-          console.log(err.response);
+          if(err.response.status === 401) {
+            MethodRequest.userLogout();
+            window.location.reload();
+          }
         });
     },
   },

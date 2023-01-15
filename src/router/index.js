a6ec61c50fce040506bfff7ca3b53254c0541cba
Ryszawy <228772@edu.p.lodz.pl>
2023-01-15 14:04:04 +0100

    Router index.js fix

--- src/router/index.js
+++ src/router/index.js
@@ -65,7 +65,7 @@ const routes = [
                         props: true
                     },
                     {
-                        path: '/suggestions',
+                        path: '/suggestionsEdit',
                         name: 'SuggestionsPanel',
                         component: SuggestionsPanel,
                         props: true
